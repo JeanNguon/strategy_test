35a4779e91058bf12afd91320feca56d55bac4e8
JeanNguon <j.nguon@groupeonepoint.com>
2017-01-19 12:53:18 +0100

    [features/connector] connector controller + connector

---
+++ test_strategy/test_strategy/Models/connector/IConnector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_strategy.Models
+{
+    interface IConnector
+    {
+        void placeOrder();
+        Int32 closeOrder();
+    }
+}
---
+++ test_strategy/test_strategy/Models/connector/connector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_strategy.Models.connector
+{
+    public class connector : IConnector
+    {
+        int IConnector.closeOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IConnector.placeOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
